e2e4ab0353a91d574b01a7480ea5ac4bcbd8c01f
vitalii-minchuk <mcmin4doutsch@gmail.com>
2024-02-12 21:56:09 +0200

    add shacdn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { Play } from "@/components/play";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function HomePage() {
   return (
     <main>
-hello
+
+<Button variant="ghost">hello</Button>
+<Play />
     </main>
   );
 }
