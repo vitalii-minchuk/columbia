ab6121cf1ff71ff0d9790c3d0303583773fce6e4
vitalii-minchuk <mcmin4doutsch@gmail.com>
2024-02-14 22:09:33 +0200

    restart project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import { Play } from "@/components/play";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
-
 export default function HomePage() {
   return (
     <main>
