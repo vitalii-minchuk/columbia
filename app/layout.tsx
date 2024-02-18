c68df5a19f4dd1415c7dca28e48ad71f0d0574e5
vitalii-minchuk <mcmin4doutsch@gmail.com>
2024-02-18 16:55:02 +0200

    add tailwind config and global css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const roboto = Roboto({ subsets: ["latin"], weight: ['400', '700'] });
+const roboto = Roboto({ subsets: ["latin"], weight: ['400', '500', '700']});
 
 export const metadata: Metadata = {
   title: "Columbia",
